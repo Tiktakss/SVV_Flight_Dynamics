26ef35ed0c6d843f0e1882b4b332e03d500a91f2
tvleming <teunvleming@gmail.com>
2019-03-06 10:14:56 +0100

    convert to clss

--- analytical_model.py
+++ analytical_model.py
@@ -1,6 +1,18 @@
 import numpy as np
+from Cit_par import Variables
 
 class Analytical_Model:
     def __init__(self):
-        self.a = 1
+        self.v_t0 = 1           #stationary normal velocity
+        self.var = Variables()
+        
+    def v_dimless(self, v_t):
+        return (v_t - self.v_t0) / self.v_t0
     
+    
+    
+    
+if __name__ == "__main__":
+    model = Analytical_Model()
+    
+    print(model.var.CXu)
