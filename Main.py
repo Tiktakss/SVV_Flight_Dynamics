857e2ae6b084f7827474184db9e7d76d707b4a8b
Tiktakss <wouter.j.spek@gmail.com>
2019-03-12 17:52:35 +0100

    main met plotjes manouvres

--- Main.py
+++ Main.py
@@ -42,8 +42,8 @@ plt.xlabel('time [min]')
 plt.ylabel('height [m]')
 
 ### ap_roll
-ap_rollstart = 60*52+56
-ap_rolltime = 20
+ap_rollstart = 60*53 + 5
+ap_rolltime = 5
 
 ap_rolldata = matlab.getdata_at_time('Ahrs1_bRollRate',ap_rollstart,ap_rollstart+ap_rolltime)/180*np.pi
 ap_rolltime = matlab.getdata_at_time('time',ap_rollstart,ap_rollstart+ap_rolltime)/60
@@ -51,19 +51,51 @@ ap_rolltime = matlab.getdata_at_time('time',ap_rollstart,ap_rollstart+ap_rolltim
 plt.figure(2)
 plt.plot(ap_rolltime,ap_rolldata)
 plt.xlabel('time [min]')
-plt.ylabel('rollrate [deg/s]')
+plt.ylabel('roll rate [rad/s]')
 
 ### sh_period
 sh_periodstart = 60*54
 sh_periodtime = 4
 
-sh_perioddata = matlab.getdata_at_time('Ahrs1_bPitchRate',sh_periodstart,sh_periodstart+sh_periodtime)
+sh_perioddata = matlab.getdata_at_time('Ahrs1_bPitchRate',sh_periodstart,sh_periodstart+sh_periodtime)/180*np.pi
 sh_periodtime = matlab.getdata_at_time('time',sh_periodstart,sh_periodstart+sh_periodtime)/60
 
 plt.figure(3)
 plt.plot(sh_periodtime,sh_perioddata)
 plt.xlabel('time [min]')
-plt.ylabel('rollrate [deg/s]')
+plt.ylabel('pitch rate [rad/s]')
+
+### dutchR
+dutchRstart = 60*56+2
+dutchRtime = 18
+
+dutchRdata = matlab.getdata_at_time('Ahrs1_bRollRate',dutchRstart,dutchRstart+dutchRtime)/180*np.pi
+dutchRdata2 = matlab.getdata_at_time('Ahrs1_bYawRate',dutchRstart,dutchRstart+dutchRtime)/180*np.pi
+dutchRtime = matlab.getdata_at_time('time',dutchRstart,dutchRstart+dutchRtime)/60
+
+plt.figure(4)
+fig, ax = plt.subplots(2, 1, sharex=True)
+ax[0].plot(dutchRtime,dutchRdata)
+ax[0].set_ylabel('roll rate [rad/s]')
+ax[1].plot(dutchRtime,dutchRdata2)
+ax[1].set_ylabel('yaw rate [rad/s]')
+ax[1].set_xlabel('time [min]')
+
+### dutchR_damp
+dutchR_dampstart = 60*57+32
+dutchR_damptime = 10
+
+dutchR_dampdata = matlab.getdata_at_time('Ahrs1_bRollRate',dutchR_dampstart,dutchR_dampstart+dutchR_damptime)/180*np.pi
+dutchR_dampdata2 = matlab.getdata_at_time('Ahrs1_bYawRate',dutchR_dampstart,dutchR_dampstart+dutchR_damptime)/180*np.pi
+dutchR_damptime = matlab.getdata_at_time('time',dutchR_dampstart,dutchR_dampstart+dutchR_damptime)/60
+
+plt.figure(5)
+fig, ax = plt.subplots(2, 1, sharex=True)
+ax[0].plot(dutchR_damptime,dutchR_dampdata)
+ax[0].set_ylabel('roll rate [rad/s]')
+ax[1].plot(dutchR_damptime,dutchR_dampdata2)
+ax[1].set_ylabel('yaw rate [rad/s]')
+ax[1].set_xlabel('time [min]')
 
 
 
