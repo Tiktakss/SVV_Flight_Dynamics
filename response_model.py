4bf4cd2f991b4a8b0bce876f445b50ead2dee740
Jeije <jeijeer@gmail.com>
2019-03-13 11:48:11 +0100

    push to wouter

--- response_model.py
+++ response_model.py
@@ -4,13 +4,14 @@ Created on Wed Mar 13 10:56:04 2019
 
 @author: Jeije
 """
-
+import control
 from control.matlab import * 
 import numpy as np
 import Cit_par as par
 from aero_tools import Aero_Tools
 from real_analytical_model import Analytical_Model
 from numerical_model import Numerical_Model
+import matplotlib.pyplot as plt
 
 class Response:
     def __init__(self):
@@ -20,7 +21,7 @@ class Response:
 
 """
 put testing/debugging code in the if-statement below
-it will only run if you run this python file (aero_tools.py)
+it will only run if you run this python file (response_model.py)
 """
 if __name__ == "__main__":
     resp = Response()
@@ -32,34 +33,23 @@ if __name__ == "__main__":
     v_tas = model.tools.ias_to_tas(alt, v_ias)
     v_dimless = model.v_dimless(v_tas, v_tas+1)
     
-    D_c = model.D_c(0.1, v_dimless)
-    D_b = model.D_b(0.1, v_dimless)
-    
-    sym = model.EOM_sym(D_c)
-    asym = model.EOM_asym(D_b)
-    
-    print()
-    print()
-    
-
-
-#    print(model.As(100))
-#    print(model.Bs(100))
-#    print()
-#    print(model.Aa(100))
-#    print(model.Ba(100))
-#    print()
-#    print(model.C())
-#    print(model.Ds())
-#    print(model.Da())
+    A=np.matrix(model.As(v_tas))
+    B=np.matrix(model.Bs(v_tas))
+    C=np.matrix(model.C())
+    D=np.matrix(model.Ds())
+    A=np.transpose(A)
+    B=np.transpose(B)
+    C=np.transpose(C)
+    D=np.transpose(D)
     
     
-#    v_ref = 1
-#    s_eigen = np.linalg.eig(model.As(v_ref))[0] / par.c
-#    print(model.amod.eigenv_short())
-#    print(model.amod.eigenv_phugoid())
-#    print('eigenvalues symm')
-#    print(s_eigen)
-#    s_eigen = np.linalg.eig(model.Aa(v_ref))[0] / par.b
-#    print('eigenvalues asymm')
-#    print(s_eigen)
+    
+    print (A.shape,B.shape,C.shape,D.shape)
+    sys=control.StateSpace(A,B,C,D)
+    x0=0
+    
+    print (sys)
+    
+    T,yout, xout=control.step_response(sys,return_x=True,transpose=True)
+    ax = plt.figure()
+    ax.plot(T,yout)
