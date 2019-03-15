c681545acbc0f8ba762ed63e3a3eab9c9a990c5a
tvleming <teunvleming@gmail.com>
2019-03-15 11:06:46 +0100

    dimensionless

--- numerical_model.py
+++ numerical_model.py
@@ -1,5 +1,5 @@
 import numpy as np
-import Cit_par as par
+import Cit_par as p
 from aero_tools import Aero_Tools
 from real_analytical_model import Analytical_Model
 #from control.matlab import * 
@@ -14,66 +14,66 @@ class Numerical_Model:
         return (v_t - v_t0) / v_t0
     
     def D_c(self, dt, v_dimless):
-        return par.c / (v_dimless * dt)
+        return p.c / (v_dimless * dt)
     
     def D_b(self, dt, v_dimless):
-        return par.b / (v_dimless * dt)
+        return p.b / (v_dimless * dt)
     
     def EOM_sym(self, D_c):
-        symm1 = [par.CXu - 2 * par.muc * D_c, par.CXa, par.CZ0, par.CXq]
-        symm2 = [par.CZu, par.CZa + (par.CZadot - 2 * par.muc)*D_c, -par.CX0, par.CZq + 2 * par.muc]
+        symm1 = [p.CXu - 2 * p.muc * D_c, p.CXa, p.CZ0, p.CXq]
+        symm2 = [p.CZu, p.CZa + (p.CZadot - 2 * p.muc)*D_c, -p.CX0, p.CZq + 2 * p.muc]
         symm3 = [0, 0, - D_c, 1]
-        symm4 = [par.Cmu, par.Cma + par.Cmadot * D_c, 0, par.Cmq - 2 * par.muc * par.KY2 * D_c]
+        symm4 = [p.Cmu, p.Cma + p.Cmadot * D_c, 0, p.Cmq - 2 * p.muc * p.KY2 * D_c]
         return np.matrix((symm1, symm2, symm3, symm4))
     
     def EOM_asym(self, D_b):
-        asymm1 = [par.CYb + (par.CYbdot - 2*par.mub)*D_b, par.CL, par.CYp, par.CYr - 4*par.mub]
+        asymm1 = [p.CYb + (p.CYbdot - 2*p.mub)*D_b, p.CL, p.CYp, p.CYr - 4*p.mub]
         asymm2 = [0, -0.5*D_b, 1, 0]
-        asymm3 = [par.Clb, 0, par.Clp - 4*par.mub * par.KX2 * D_b, par.Clr + 4*par.mub * par.KXZ * D_b]
-        asymm4 = [par.Cnb + par.Cnbdot, 0, par.Cnp + 4*par.mub * par.KXZ * D_b, par.Cnr - 4*par.mub * par.KZ2 * D_b]
+        asymm3 = [p.Clb, 0, p.Clp - 4*p.mub * p.KX2 * D_b, p.Clr + 4*p.mub * p.KXZ * D_b]
+        asymm4 = [p.Cnb + p.Cnbdot, 0, p.Cnp + 4*p.mub * p.KXZ * D_b, p.Cnr - 4*p.mub * p.KZ2 * D_b]
         return np.matrix((asymm1, asymm2, asymm3, asymm4))
     
     def elev_defl_mat(self):
-        vect = [-par.CXde, -par.CZde, 0, -par.Cmde]
+        vect = [-p.CXde, -p.CZde, 0, -p.Cmde]
         #print(np.transpose(vect))
         return np.transpose(vect)
 
     def Ps(self, v_t0):
-        P1 = [-2 * par.muc * par.c / v_t0**2, 0, 0, 0]
-        P2 = [0, (par.CZadot - 2 * par.muc) * par.c / v_t0, 0, 0]
-        P3 = [0, 0, -par.c / v_t0, 0]
-        P4 = [0, par.Cmadot * par.c / v_t0, 0, -2 * par.muc * par.KY2 * par.c**2 / v_t0**2]
+        P1 = [-2 * p.muc * p.c / v_t0,      0,                                      0,              0]
+        P2 = [0,                            (p.CZadot - 2 * p.muc) * p.c / v_t0,    0,              0]
+        P3 = [0,                            0,                                      -p.c / v_t0,    0]
+        P4 = [0,                            p.Cmadot * p.c / v_t0,                  0,              -2 * p.muc * p.KY2 * p.c / v_t0]
         #print(np.matrix((P1, P2, P3, P4)))
         return np.matrix((P1, P2, P3, P4))
 
     def Qs(self, v_t0):
-        Q1 = [-par.CXu/v_t0, -par.CXa, -par.CZ0, -par.CXq * par.c/v_t0]
-        Q2 = [-par.CZu/v_t0, -par.CZa, par.CX0, -(par.CZq + 2 * par.muc)* par.c/v_t0]
-        Q3 = [0, 0, 0, -1* par.c/v_t0]
-        Q4 = [-par.Cmu/v_t0, -par.Cma, 0, -par.Cmq* par.c/v_t0]
+        Q1 = [-p.CXu,  -p.CXa,     -p.CZ0,     0]
+        Q2 = [-p.CZu,  -p.CZa,     p.CX0,      -(p.CZq + 2 * p.muc)]
+        Q3 = [0,       0,          0,          -1]
+        Q4 = [-p.Cmu,  -p.Cma,     0,          -p.Cmq]
         #print(np.matrix((Q1, Q2, Q3, Q4)))
         return np.matrix((Q1, Q2, Q3, Q4))
 
 
     def Pa(self, v_t0):
-        P1 = [(par.CYbdot -2 * par.mub)*par.b/v_t0, 0, 0, 0]
-        P2 = [0, -0.5*par.b/v_t0, 0, 0]
-        P3 = [0, 0, -4 * par.mub * par.KX2 * par.b/v_t0, 4 * par.mub * par.KXZ * par.b/v_t0]
-        P4 = [par.Cnbdot * par.b/v_t0, 0, 4 * par.mub * par.KXZ * par.b/v_t0, -4 * par.mub * par.KZ2 * par.b/v_t0]
+        P1 = [(p.CYbdot -2 * p.mub)*p.b/v_t0, 0, 0, 0]
+        P2 = [0, -0.5*p.b/v_t0, 0, 0]
+        P3 = [0, 0, -4 * p.mub * p.KX2 * p.b/v_t0, 4 * p.mub * p.KXZ * p.b/v_t0]
+        P4 = [p.Cnbdot * p.b/v_t0, 0, 4 * p.mub * p.KXZ * p.b/v_t0, -4 * p.mub * p.KZ2 * p.b/v_t0]
         return np.matrix((P1, P2, P3, P4))
     
     def Qa(self):
-        Q1 = [-par.CYb, -par.CL, -par.CYp, -(par.CYr - 4*par.mub)]
+        Q1 = [-p.CYb, -p.CL, -p.CYp, -(p.CYr - 4*p.mub)]
         Q2 = [0, 0, -1, 0]
-        Q3 = [-par.Clb, 0, -par.Clp, -par.Clr]
-        Q4 = [-par.Cnb, 0, -par.Cnp, -par.Cnr]
+        Q3 = [-p.Clb, 0, -p.Clp, -p.Clr]
+        Q4 = [-p.Cnb, 0, -p.Cnp, -p.Cnr]
         return np.matrix((Q1, Q2, Q3, Q4))
     
     def Ra(self):
-        R1 = [-par.CYda, -par.CYdr, ]
+        R1 = [-p.CYda, -p.CYdr, ]
         R2 = [0, 0]
-        R3 = [-par.Clda, -par.Cldr]
-        R4 = [-par.Cnda, -par.Cndr]
+        R3 = [-p.Clda, -p.Cldr]
+        R4 = [-p.Cnda, -p.Cndr]
         return np.matrix((R1, R2, R3, R4))
     
     def As(self, v_t0):
@@ -127,7 +127,7 @@ class Numerical_Model:
             X_s += DX_s*self.delta_t
         return u_hat, AoA, Theta, q
 
-    def Xs(self,manouvre):
+#    def Xs(self,manouvre):
         
 
         
@@ -160,21 +160,23 @@ if __name__ == "__main__":
 
     As_mat=model.As(v_ref)
 #    As_mat[:,0]*=1/v_ref
-#    As_mat[:,-1]*=par.c/v_ref
-    As_eig=np.linalg.eig(As_mat)[0]
+#    As_mat[:,-1]*=p.c/v_ref
+    As_eig=np.linalg.eig(As_mat)[0] * v_ref/p.c
     
     Aa_mat=model.Aa(v_ref)
-#    Aa_mat[:,-1]*=par.b*0.5/v_ref
-#    Aa_mat[:,-2]*=par.b*0.5/v_ref
+#    Aa_mat[:,-1]*=p.b*0.5/v_ref
+#    Aa_mat[:,-2]*=p.b*0.5/v_ref
     Aa_eig=np.linalg.eig(Aa_mat)[0]
     
     print(As_mat)
     print(As_eig)
-    print(model.amod.half_time(np.real(As_eig),v_ref))
-    print()
-    print(Aa_mat)
-    print(Aa_eig)
-    print(model.amod.half_time2(np.real(Aa_eig),v_ref))
+#    print(model.amod.period_s(np.imag(As_eig),v_ref))
+#    print()
+#    print(Aa_mat)
+#    print(Aa_eig)
+#    print(model.amod.half_time2(np.real(Aa_eig),v_ref))
+    
+    
 #    print(model.Ba(1))
 #    print()
 #    print(model.C())
@@ -183,12 +185,12 @@ if __name__ == "__main__":
     
     
     
-    s_eigen = np.linalg.eig(model.As(v_ref))[0] / par.c
+    s_eigen = np.linalg.eig(model.As(v_ref))[0] / p.c
 #    print(model.amod.eigenv_short())
 #    print(model.amod.eigenv_phugoid())
 #    print('eigenvalues symm')
 #    print(s_eigen)
-#    s_eigen = np.linalg.eig(model.Aa(v_ref))[0] / par.b
+#    s_eigen = np.linalg.eig(model.Aa(v_ref))[0] / p.b
 #    print('eigenvalues asymm')
 #    print(s_eigen)
 
