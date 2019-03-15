d3d0a7acca31b358c604dec3bc83dfb3d5dc4474
Rolijne <47812774+Rolijne@users.noreply.github.com>
2019-03-15 11:43:03 +0100

    Update

--- Cl_Cd_Plot.py
+++ Cl_Cd_Plot.py
@@ -12,7 +12,7 @@ from matlab_tools import Matlab_Tools
 matlab = Matlab_Tools('./FTISxprt-20190305_124649.mat')
 
 #inputs
-blockfuel       = 4090.0 #lbs #obtained from written data sheet
+blockfuel       = 4050.0 #lbs #obtained from written data sheet
 empty_weight    = 9165.0 #lbs #obtained from Mass & Balance report
 gravity = par.g #m/s^2
 surface = par.S #m^2 #obtained from reader
@@ -20,23 +20,21 @@ Cd0 = par.CD0 # #obtained from reader
 e = par.e #obtained from reader
 b = par.b #span obtained from reader #m
 length = 600 *0.0254
+A = b*b/surface #aspect ratio
 
 
 #obtain data for Cl-Cd plot
-data = import_excel.Cl_Cd_data(excel)
-
 weight_people = sum(import_excel.weights(excel))/0.453592 #lbs
-
 total_weight = weight_people + blockfuel + empty_weight
 
 #time stamps plots
-t1 = 18*60 + 50 #seconds
-t2 = 21*60 + 32 #seconds
-t3 = 25*60 + 31 #seconds
-t4 = 27*60 + 29 #seconds
-t5 = 29*60 + 3  #seconds
-t6 = 31*60 + 13 #seconds
-dt = 2          #seconds
+t1 = 18*60 + 50. #seconds
+t2 = 21*60 + 33. #seconds
+t3 = 25*60 + 30. #seconds
+t4 = 27*60 + 29. #seconds
+t5 = 29*60 + 3.  #seconds
+t6 = 31*60 + 14. #seconds
+dt = 0.5          #seconds
 
 time = [t1, t2, t3, t4, t5, t6]
 
@@ -44,13 +42,12 @@ time = [t1, t2, t3, t4, t5, t6]
 C_l = [] #lift coefficient
 C_d = [] #drag coefficient
 alpha = [] #angle of attack
-mach = []
-Re = []
+C_d_update = []
 
 for i in range(len(time)):
-    fuel_left = matlab.getdata_at_time('lh_engine_FU',time[i], time[i]+dt)
+    fuel_left = matlab.getdata_at_time('lh_engine_FU',time[i], time[i] + dt)
     fuel_left_avg = (sum(fuel_left))/(len(fuel_left))
-    fuel_right = matlab.getdata_at_time('rh_engine_FU',time[i], time[i]+dt)
+    fuel_right = matlab.getdata_at_time('rh_engine_FU',time[i], time[i] + dt)
     fuel_right_avg = (sum(fuel_right))/(len(fuel_right))
     fuel_total = fuel_left_avg + fuel_right_avg
 
@@ -58,40 +55,42 @@ for i in range(len(time)):
     weight_kg = weight_lbs * 0.453592 #weight in kg
     weight_n = weight_kg*gravity #weight inNewon
     
-    height = matlab.getdata_at_time('Dadc1_alt', time[i], time[i]+dt)
+    height = matlab.getdata_at_time('Dadc1_alt', time[i], time[i ]+ dt)
     h_ft = (sum(height))/(len(height))
     h_m = aero.ft_to_m(h_ft) #height in m
     density = aero.rho_alt(h_m) #density kg/m^3
     
-    speed = matlab.getdata_at_time('Dadc1_tas', time[i], time[i]+dt)
+    speed = matlab.getdata_at_time('Dadc1_tas', time[i], time[i] + dt)
     speed_kts = (sum(speed))/(len(speed))
     speed_ms = aero.kts_to_ms(speed_kts)
     
     lift = (2*weight_n)/(density*surface*speed_ms**2)
     C_l.append(lift)
     
-    A = b*b/surface #aspect ratio
     drag = Cd0 +(lift**2)/(pi*A*e)
     C_d.append(drag)
     
-    angle = matlab.getdata_at_time('vane_AOA', time[i], time[i] +dt)
+    angle = matlab.getdata_at_time('vane_AOA', time[i], time[i] + dt)
     aoa = (sum(angle))/(len(angle)) #angle of attack #degrees
     alpha.append(aoa)
-    
-    M = aero.calc_mach(h_m, speed_ms)
-    mach.append(M)
-    
-    re_num = aero.calc_re(density, speed_ms, length)
-    Re.append(re_num)
+
+#Update OSwald factor
+a  = (C_l[-1] - C_l[0])/(alpha[-1] - alpha[0])
+a0 = 0.1097
+e_better = 1/(pi*A) * 57.3 * a0 /(a0/a -1)
+
+for i in range(len(time)): 
+    drag_new = Cd0 +(C_l[i]**2)/(pi*A*e_better)
+    C_d_update.append(drag_new)
 
 
 #Trim Curve
-t1_trim = 34*60 + 9
+t1_trim = 34*60 + 8
 t2_trim = 36*60 + 50
 t3_trim = 38*60 + 0
-t4_trim = 39*60 + 13
+t4_trim = 39*60 + 12
 t5_trim = 40*40 + 40
-t6_trim = 41*60 + 54
+t6_trim = 41*60 + 56
 time_trim = [t1_trim, t2_trim, t3_trim, t4_trim, t5_trim, t6_trim]
 
 trim_curve = []
@@ -101,23 +100,22 @@ speed_lst = []
 
 
 for i in range(len(time_trim)):
-    elevator = matlab.getdata_at_time('delta_e', time_trim[i], time_trim[i] + dt)
+    elevator = matlab.getdata_at_time('delta_e', time_trim[i], time_trim[i ]+ dt)
     elevator_avg = (sum(elevator))/(len(elevator))
     trim_curve.append(elevator_avg)
     
-    angle_trim = matlab.getdata_at_time('vane_AOA', time_trim[i], time_trim[i] + dt )
+    angle_trim = matlab.getdata_at_time('vane_AOA', time_trim[i], time_trim[i] + dt)
     angle_avg = (sum(angle_trim))/(len(angle_trim))
     alpha_trim.append(angle_avg)
     
-    speed_trim = matlab.getdata_at_time('Dadc1_tas', time_trim[i], time_trim[i]+dt)
+    speed_trim = matlab.getdata_at_time('Dadc1_tas', time_trim[i], time_trim[i ] + dt)
     speed_kts_trim = (sum(speed_trim))/(len(speed_trim))
     speed_ms_trim = aero.kts_to_ms(speed_kts_trim)
     speed_lst.append(speed_ms_trim)
-
     
 #plot trim curve
 plt.subplot(221)
-plt.plot(alpha_trim, trim_curve, "ro")
+plt.plot(alpha_trim, trim_curve, "ro" , label='Flight Data')
 plt.title('Trim Curve')
 plt.xlabel('Angle of Attack [-]')
 plt.ylabel('Delta_e [-]')
@@ -125,7 +123,7 @@ plt.grid(True)
 
 z = np.polyfit(alpha_trim, trim_curve, 1)
 p = np.poly1d(z)
-plt.plot(alpha_trim,p(alpha_trim),"r--")
+plt.plot(alpha_trim,p(alpha_trim),"b", label='Linear Regression')
 
 plt.subplot(222)
 plt.plot(speed_lst, trim_curve, "ro")
@@ -136,27 +134,32 @@ plt.grid(True)
     
 #Plot Cl_CD Curve
 plt.subplot(223)
-plt.plot(C_d[0], C_l[0], "ro")
-plt.plot(C_d[1], C_l[1], "bo")
-plt.plot(C_d[2], C_l[2], "go")
-plt.plot(C_d[3], C_l[3], "yo")
-plt.plot(C_d[4], C_l[4], "ko")
-plt.plot(C_d[5], C_l[5], "co")
-plt.plot(C_d, C_l)
-plt.text(0.045, 0.65, r'$M=0.172 - 0.3473,\ \Re= $')
+plt.plot(C_d, C_l, "ro", label='Orginal')
+plt.plot(C_d_update, C_l, "bo", label='Updated')
 plt.title('Lift coefficient vs Drag coefficient')
 plt.xlabel('Drag coefficient [-]')
 plt.ylabel('Lift coefficient [-]')
 plt.grid(True)
 
+z1 = np.polyfit(C_d, C_l, 1)
+p1 = np.poly1d(z1)
+plt.plot(C_d,p1(C_d),"r--", label='Linear Regression')
+
 #PLot lift curve
 plt.subplot(224)
-plt.plot(alpha, C_l)
+plt.plot(alpha, C_l, "ro", label='FLight Data')
 plt.title('Lift Curve')
 plt.xlabel('Angle of Attack [-]')
 plt.ylabel('Lift coefficient [-]')
 plt.grid(True)
+
+z2 = np.polyfit(alpha, C_l, 1)
+p2 = np.poly1d(z2)
+plt.plot(alpha,p2(alpha),"r--", label='Linear Regression')
 plt.show()
+
+
+
 
     
 
