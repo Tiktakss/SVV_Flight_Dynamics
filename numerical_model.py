5743f6e39ceb1f5f92b9bc67e5ffe4d236f9dfe1
Jeije <jeijeer@gmail.com>
2019-03-20 10:35:36 +0100

    d

--- numerical_model.py
+++ numerical_model.py
@@ -4,7 +4,8 @@ from aero_tools import Aero_Tools
 from real_analytical_model import Analytical_Model
 from matlab_tools import Matlab_Tools
 matlab = Matlab_Tools('FTISxprt-20190305_124649.mat')
-from control.matlab import * 
+#from control.matlab import * 
+import scipy.signal as signal
 
 class Numerical_Model:
     def __init__(self):
@@ -148,7 +149,7 @@ class Numerical_Model:
     
     
     
-    def not_symmetric_interpolate(self,manouvre):
+    def old_not_symmetric_interpolate(self,manouvre):
         start,time = matlab.gettimes(manouvre)
         
         Xa, vtas = matlab.Xa(manouvre)
@@ -179,7 +180,32 @@ class Numerical_Model:
     def integrate(self,array):
         return np.array([(array[i+1]-array[i])/self.delta_t for i in range(len(array)-1)])
 
-
+    def not_symmetric_control(self,manouvre):
+        start,time = matlab.gettimes(manouvre)
+        
+        Xa, vtas = matlab.Xa(manouvre)
+        da = matlab.getdata_at_time('delta_a',start,start+time)
+        dr = matlab.getdata_at_time('delta_r',start,start+time)
+        vt0 = matlab.getdata_at_time('Dadc1_tas',start,start+0.2)[0]
+        Beta=np.array(Xa)[0]
+        Phi=np.array(Xa)[1]
+        pbover2v=np.array(Xa)[2]
+        rbover2v=np.array(Xa)[3]
+        for t in range(1,len(self.t_run(time))):
+            U_a = np.transpose(np.matrix([da[t],dr[t]]))
+            if __name__ == "__main__":
+                print ('8======D')#,Xa)
+            DX_a = np.dot(self.Aa(vt0),(Xa)) + (self.Ba(vt0)*U_a)
+            Xa = Xa + DX_a*self.delta_t
+            Beta = np.vstack((Beta,Xa[0]))
+            Phi = np.vstack((Phi,Xa[1]))
+            pbover2v = np.vstack((pbover2v,Xa[2]))
+            rbover2v = np.vstack((rbover2v,Xa[3]))
+        Beta = np.array(Beta)
+        Phi = np.array(Phi)
+        pbover2v = np.array(pbover2v)
+        rbover2v = np.array(rbover2v)
+        return Beta, Phi, pbover2v, rbover2v
         
 if __name__ == "__main__":
     model = Numerical_Model()
